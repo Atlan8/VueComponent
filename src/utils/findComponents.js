2ec12953145b5969321e9f4ab6f5b62bd9bb79a5
Atlan <756777965@qq.com>
2023-10-09 23:18:05 +0800

    style: 格式化代码

--- src/utils/findComponents.js
+++ src/utils/findComponents.js
@@ -1,12 +1,11 @@
-
 export const findComponentUpward = (context, componentName) => {
-    let parent = context.$parent
-    let name = parent.$options.name
+  let parent = context.$parent;
+  let name = parent.$options.name;
 
-    while (parent && (!name || [componentName].indexOf(name) < 0)) {
-        parent = parent.$parent
-        if (parent) name =  parent.$options.name
-    }
+  while (parent && (!name || [componentName].indexOf(name) < 0)) {
+    parent = parent.$parent;
+    if (parent) name = parent.$options.name;
+  }
 
-    return parent
-}
+    return parent;
+};
